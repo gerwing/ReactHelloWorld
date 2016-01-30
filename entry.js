606582cd059be8dfe29199780e122f5e39a48a48
Gerwin Glorieux <gerwin.glorieux@kcl.ac.uk>
2016-01-30 19:30:08 +0000

    First react example

--- entry.js
+++ entry.js
@@ -1,3 +1,7 @@
-require("./styles.css");
-document.write(require("./content.js"));
-document.write("hellowkes");
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('example')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,15 @@ module.exports = {
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style!css" }
+            { test: /\.css$/, loader: "style!css" },
+            {
+              test: /\.js?$/,
+              exclude: /(node_modules|bower_components)/,
+              loader: 'babel', // 'babel-loader' is also a legal name to reference
+              query: {
+                presets: ['react', 'es2015']
+              }
+            }
         ]
     }
 };
